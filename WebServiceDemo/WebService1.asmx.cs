a04031bf63ffe63418f7b9d5f81b18c50d5f45e3
SandrineMang'ong'oMa <SandrineMang'ong'oMa@JITU-MP2EJD1L>
2023-12-01 16:44:05 +0300

    Add project files.

---
+++ WebServiceDemo/WebService1.asmx.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web.Services;
+
+namespace WebServiceDemo
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod(MessageName="Add2Numbers")]
+        public int Add(int firstNumber, int secondNumber)
+        {
+
+            return firstNumber + secondNumber;
+        }
+
+        [WebMethod]
+        public int Add(int firstNumber, int secondNumber, int thirdNumber)
+        {
+
+            return firstNumber + secondNumber + thirdNumber;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public List<string> GetCalculations()
+        {
+            if (Session["CALCULATIONS"] == null){
+                List<string> calculations = new List<string>();
+                calculations.Add("You have not performed any calculations");
+                return calculations;
+            }
+            else
+            {
+                return (List<string>)Session["CALCULATIONS"];
+            }
+        }
+    }
+}
